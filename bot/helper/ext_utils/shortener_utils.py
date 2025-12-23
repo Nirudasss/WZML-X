fd3e4143fa4b4eec0b501b3d3a62dcffe868eb5e
Nirudasss <swamibaba079@gmail.com>
2025-12-23 16:10:38 +0530

    Implement get_encrypted_url function
    Added a function to get encrypted URLs using an external API.
    
    Signed-off-by: Nirudasss <swamibaba079@gmail.com>

--- bot/helper/ext_utils/shortener_utils.py
+++ bot/helper/ext_utils/shortener_utils.py
@@ -1,3 +1,4 @@
+import requests
 from base64 import b64encode
 from random import choice, random
 from asyncio import sleep as asleep
@@ -9,15 +10,31 @@ from urllib3 import disable_warnings
 from ... import LOGGER, shortener_dict
 from ...core.config_manager import Config
 
+def get_encrypted_url(link, site='', api=''):
+    params = {'url': link}
+    if site and api:
+        params['site'] = site
+        params['api'] = api
+    elif site and not api:
+        raise ValueError("api is missing")
+    elif api and not site:
+        raise ValueError("site is missing")
+
+    res = requests.get("https://short.gkbotz.qzz.io/api/encrypt", params=params)
+    if res.status_code == 200:
+        return res.json().get('encrypted_url', link)
+
 
 async def short_url(longurl, attempt=0):
     if not shortener_dict and not Config.PROTECTED_API:
         return longurl
     if attempt >= 4:
         return longurl
+        if True and (encrypted_url_ :=  get_encrypted_url(longurl, _shortener, _shortener_api)):
+                return encrypted_url_
 
     cget = create_scraper().request
-    disable_warnings()
+    disable_warning 
     try:
         if Config.PROTECTED_API:
             res = cget("GET", Config.PROTECTED_API, params={"url": longurl}).json()
