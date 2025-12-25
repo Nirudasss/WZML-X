f07e8c45078df885045a155a5e09058167052bbc
Nirudasss <swamibaba079@gmail.com>
2025-12-25 14:45:05 +0530

    Remove get_encrypted_url function
    Removed the get_encrypted_url function and its usage in short_url.
    
    Signed-off-by: Nirudasss <swamibaba079@gmail.com>

--- bot/helper/ext_utils/shortener_utils.py
+++ bot/helper/ext_utils/shortener_utils.py
@@ -10,29 +10,12 @@ from urllib3 import disable_warnings
 from ... import LOGGER, shortener_dict
 from ...core.config_manager import Config
 
-async def get_encrypted_url(link, site='', api=''):
-    params = {'url': link}
-    if site and api:
-        params['site'] = site
-        params['api'] = api
-    elif site and not api:
-        raise ValueError("api is missing")
-    elif api and not site:
-        raise ValueError("site is missing")
-
-    res = requests.get("https://short.gkbotz.qzz.io/api/encrypt", params=params)
-    await asleep(0)
-    if res.status_code == 200:
-        return res.json().get('encrypted_url', link)  
-
 async def short_url(longurl, attempt=0):
     if not shortener_dict and not Config.PROTECTED_API:
         return longurl
     if attempt >= 4:
         return longurl
-    if True and (encrypted_url_ := await get_encrypted_url(longurl, _shortener, _shortener_api)):
-                return encrypted_url_
-        
+    
     cget = create_scraper().request
     disable_warning 
     try:
